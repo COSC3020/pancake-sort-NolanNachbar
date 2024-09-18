868a2ea74996ee84874a4bee4abc6c26b8ebd7ea
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-18 15:40:18 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,25 +3,14 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
 
-// const testSort =
-//     jsc.forall("array nat", function(arr) {
-//         var a1 = JSON.parse(JSON.stringify(arr));
-//         var a2 = JSON.parse(JSON.stringify(arr));
-//         return JSON.stringify(pancakeSort(a1)) ==
-//             JSON.stringify(a2.sort(function(a, b)
-//                 { return a - b; }));
-//     });
-const testSort = jsc.forall("array nat", function(arr) {
-    var a1 = JSON.parse(JSON.stringify(arr));
-    var a2 = JSON.parse(JSON.stringify(arr));
-    const sorted = a2.sort(function(a, b) { return a - b; });
-    const result = pancakeSort(a1);
-    console.log("Original array:", arr);
-    console.log("Sorted array:", sorted);
-    console.log("Result:", result);
-    return JSON.stringify(result) === JSON.stringify(sorted);
-});
-
+const testSort =
+    jsc.forall("array nat", function(arr) {
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = JSON.parse(JSON.stringify(arr));
+        return JSON.stringify(pancakeSort(a1)) ===
+            JSON.stringify(a2.sort(function(a, b)
+                { return a - b; }));
+    });
 
 jsc.assert(testSort);
 
